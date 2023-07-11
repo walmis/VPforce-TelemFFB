0496b991e11768f1b11cd89edae36e8dbb347401
mjfrisby <98243082+mjfrisby@users.noreply.github.com>
2023-07-10 22:34:34 -0400

    Implement Jet Engine Rumble

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import time
 sys.path.insert(0, '') 
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     stream=sys.stdout,
