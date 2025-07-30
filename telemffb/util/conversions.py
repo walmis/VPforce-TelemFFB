3c04e80a2fdf37def81f9826a33190fef4adc31b
ValmantasP <walmis@gmail.com>
2025-07-30 06:00:45 -0700

    add conversions.py

---
+++ telemffb/util/conversions.py
@@ -0,0 +1,12 @@
+import math
+
+deg = 180 / math.pi
+slugft3 = 0.00194032  # SI to slugft3
+rad = 0.0174532925
+ft = 3.28084  # m to ft
+kt = 1.94384  # ms to kt
+kt2ms = 0.514444  # knots to m/s
+ms2kt = 1.943844  # m/s to knot
+vsound = 290.07 # m/s, speed of sound at sea level in ISA condition
+P0 = 101325 # Pa, ISA static pressure at sealevel
+std_air_pressure = 1.225  # kg/m^3
