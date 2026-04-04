b7e0ce813df5265d21ae5d5b3367f4bdeed9091f
Micah Frisby <mjfrisby@icloud.com>
2026-04-03 21:12:52 -0400

    allow float or list for aileron position AP following

--- telemffb/sim/msfs_xp/MsfsXpFBWFlightControlsMixIn.py
+++ telemffb/sim/msfs_xp/MsfsXpFBWFlightControlsMixIn.py
@@ -120,14 +120,12 @@ class MsfsXpFBWFlightControlsMixIn(AdvancedSpringMixIn, MsfsXpSimConnectMixIn):
                         virtual_stick_y_offs = elevator_pos - (elevator_pos * self.joystick_trim_follow_gain_virtual_y)
                         phys_stick_y_offs = int(elevator_pos * 4096)
 
-                        aileron_pos = clamp(aileron_pos[0] * self.joystick_ap_follow_gain_physical_x, -1, 1)
-                        # aileron_pos = self.dampener.dampen_value(aileron_pos, '_aileron_pos', derivative_hz=5, derivative_k=0.15)
-                        virtual_stick_x_offs = aileron_pos - (aileron_pos * self.joystick_ap_follow_gain_virtual_x)
+                        if isinstance(aileron_pos, list):
+                            aileron_pos = aileron_pos[0]
 
-                        # if self.joystick_ap_y_follow_axis:
-                        #     elevator_pos = clamp(elevator_pos * self.joystick_ap_follow_gain_physical_y, -1, 1)
-                        #     virtual_stick_y_offs = elevator_pos - (elevator_pos * self.joystick_ap_follow_gain_virtual_y)
-                        #     phys_stick_y_offs = round(elevator_pos * 4096)
+                        aileron_pos = clamp(aileron_pos * self.joystick_ap_follow_gain_physical_x, -1, 1)
+
+                        virtual_stick_x_offs = aileron_pos - (aileron_pos * self.joystick_ap_follow_gain_virtual_x)
 
                         ap_send_flag_x = (
                             True if abs(phys_x - aileron_pos) > self.joystick_ap_x_follow_deadzone else False
